69bbe72c2f6bd749b7b7f12d6e8ae486554f352f
Tomasz <tomaszburas.dev@gmail.com>
2024-02-02 13:54:32 +0100

    Set noImplicitAny: false

--- middlewares/authenticate-jwt.ts
+++ middlewares/authenticate-jwt.ts
@@ -2,11 +2,7 @@ import passport from 'passport';
 import { NextFunction, Request, Response } from 'express';
 import { AuthError } from './handle-error';
 
-export const authenticateJwt = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
+export const authenticateJwt = (req: Request, res: Response, next: NextFunction) => {
     passport.authenticate('jwt', { session: false }, (err, user, info) => {
         if (err) return next(err);
         if (!user) throw new AuthError();
